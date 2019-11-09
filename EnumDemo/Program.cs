1904376d34ada5919663cb2b688d43b25a0b5876
Иван Иван <remillius_dies@outlook.com>
2019-11-09 14:19:53 +0300

    Add project files.

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3D c = new Vector3D
+            {
+                X = 34, Y = 4, Z = 243
+            };
+
+            foreach (double coordinate in c)
+            {
+                Console.WriteLine(coordinate);
+            }
+
+            Console.WriteLine();
+
+            foreach (int item in Odd())
+            {
+                Console.WriteLine(item);
+                if (Console.KeyAvailable)
+                {
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> Odd()
+        {
+            for (int i = 1; i <= int.MaxValue; i=i+2)
+            {
+                yield return i;
+            }
+            
+        }
+    }
+}
+ 
---
+++ EnumDemo/Vector3D.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    public class Vector3D : IEnumerable<double>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public IEnumerator<double> GetEnumerator()
+        {
+            yield return X;
+            yield return Y;
+            yield return Z;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class VectorEnumerator: IEnumerator<double>
+    {
+        private int _index=0;
+        private readonly Vector3D _vector;
+        public void Dispose()
+        {
+        }
+
+        public VectorEnumerator(Vector3D vector)
+        {
+            _vector = vector;
+        }
+        public bool MoveNext()
+        {
+            if (_index < 3)
+            {
+                _index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Current
+        {
+            get
+            {
+                if (_index == 1) return _vector.X; 
+                if (_index == 2) return _vector.Y; 
+                if (_index == 3) return _vector.Z;
+                return 0;
+            }
+        }
+
+        object IEnumerator.Current => Current;
+    }
+}
